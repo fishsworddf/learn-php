501922047b75c8b1eb968fc9196e3c72cf770b76
shenjie <jieshencx@meilishuo.com>
2016-10-13 16:54:17 +0800

    约瑟夫算法 猴子选王

---
+++ monkey.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @param int $n 总数
+ * @param int $m 出局编号
+ * @return array
+ */
+function monkey($n, $m) {
+    if ($n <= 1) {
+        return '数量必须大于1';
+    }
+    $arr = range(1,$n);
+    $i = 0; //设置数组指针
+    while(count($arr) > 1) {
+        if (($i + 1) % $m == 0) {
+            unset($arr[$i]);
+        } else {
+            array_push($arr, $arr[$i]); //如果没数到n或n的倍数,则把该猴放回尾部.
+            unset($arr[$i]);
+        }
+        $i++;
+    }
+    return $arr;
+}
+$king = monkey(5,2);
+var_dump($king);
